b5db8d4e5419ec23412fd7381f5b33d588dd860f
lukas <lukas.loosli@protonmail.com>
2021-11-12 10:23:10 +0100

    added material support and tested it with slider

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -5,8 +5,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   imports: [
@@ -14,8 +15,10 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule, 
+    MatFormFieldModule, 
+    MatSliderModule
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule { }
