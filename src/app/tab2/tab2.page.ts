7689bd3544d7fb2126b6206e98994e02217fecd6
8O0 <lukas.loosli@hotmail.de>
2021-12-16 17:11:49 +0100

    added logical formula to the graph, commented code

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,16 +8,19 @@ import { Color, Label } from 'ng2-charts';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-  value1: number = 0;
-  value2: number = 0;
-  value3: number = 0;
-  value4: number = 0;
 
-  valueStnd1: number = 80;
-  valueStnd2: number = 130;
-  valueStnd3: number = 90;
-  valueStnd4: number = 95;
+export class Tab2Page {
+  /** 
+   * Sliderwerte 
+   */
+  carbohydrateSlider: number = 20;
+  unitsSlider: number = 1;
+  dailyActivity: number = 0;
+
+  /** 
+   * Standardwerte einer Normalkurve in mg/dl
+   */
+  valueStnd: number = 90;
 
 
   ngOnInit() {
@@ -26,75 +29,99 @@ export class Tab2Page {
 
   constructor() { }
 
-  formatLabel1(value: number) {
-    this.valueStnd1 = value;
-
+  /** 
+   * Updated das Bubbleicon auf dem Slider und addiert die Einheit 'g' für Gramm hinzu 
+   * @param  {Number} value Der Wert des Sliders
+   * @return {String}       Der Wert des Sliders mit einem 'g'  
+  */
+  formatLabel1(value: number): string {
     return value + 'g';
   }
-  formatLabel2(value: number) {
-    this.valueStnd2 = value;
 
+  /** 
+   * Updated das Bubbleicon auf dem Slider 
+   * @param  {Number} value Der Wert des Sliders
+   * @return {Number}       Der Wert des Sliders
+  */
+  formatLabel2(value: number): number {
     return value;
   }
-  formatLabel3(value: number) {
-    this.valueStnd3 = value;
-    let sign = value < 0 ?
-      "-" :
-      "";
+
+  /** 
+   * Updated das Bubbleicon auf dem Slider und formatiert es in Minuten und Sekunden um
+   * @param  {Number} value Der Wert des Sliders
+   * @return {String}       Der Wert des Sliders im Format HH:mm
+  */
+  formatLabel3(value: number): string {
     let absoluted = Math.abs(value);
     let min = Math.floor(absoluted);
     let sec = Math.floor(absoluted * 60) % 60;
-    let timeInHours = sign +
-      (min < 10 ? "0" : "") +
-      min +
-      ":" +
-      (sec < 10 ? "0" : "") +
-      sec;
+
+    // In den ternären Operatoren wird eine führende Null angefügt wenn der Wert nicht zweistellig ist 
+    let timeInHours = (min < 10 ? "0" : "") + min + ":" + (sec < 10 ? "0" : "") + sec;
 
     return timeInHours;
   }
 
+  /** 
+   * Überträgt jegliche Slidermanipulationen in das Chart
+  */
   refreshData() {
-    console.log('value1', this.value1, 'value2', this.value2, 'value3', this.value3, 'value4', this.value4);
-    console.log('valueStnd1', this.value1, 'valueStnd2', this.value2, 'valueStnd3', this.value3, 'valueStnd4', this.valueStnd4);
+    let cfCarbs = 2.5 // Correction Factor Carbohydrates. Der Faktor um eine Simulation zu ermöglichen
+    let cfUnits = 2.9 // Correction Factor Carbohydrates. Der Faktor um eine Simulation zu ermöglichen
+    let cfActivity = 15 // Correction Factor Carbohydrates. Der Faktor um eine Simulation zu ermöglichen
+
+    let carbValue = this.carbohydrateSlider * cfCarbs
+    let unitValue = this.unitsSlider * cfUnits
+    let activityValue = this.dailyActivity * cfActivity
+
+    let valuePerHour = carbValue - unitValue - activityValue
+
+    console.log("ValPerHour", valuePerHour)
+    console.log("KH:", carbValue)
+    console.log("Units:", unitValue)
+    console.log("DailyActivity:", activityValue)
+
+    // Initialisiert und berechnet die 5 Punkte auf dem Chart.
+    // Die Werte sind logisch gewählt um eine zeitliche Beeinflussung zu simulieren
     this.lineChartData[0].data = [
-      this.valueStnd1-this.value1,
-      this.valueStnd2-this.value2,
-      this.valueStnd3-this.value3,
-      this.valueStnd4-this.value4,
+      (valuePerHour * 0.2) + this.valueStnd,
+      (valuePerHour * 1.2) + this.valueStnd,
+      (valuePerHour * 0.8) + this.valueStnd,
+      (valuePerHour * -0.2) + this.valueStnd,
+      (valuePerHour * -0.1) + this.valueStnd,
+      (valuePerHour * 0.1) + this.valueStnd,
     ]
   }
 
+  // initialisiert den Chart beim ersten Anwählen des Moduls
+  // Die Werte sind anhand einer normalen Kurve gewählt
   lineChartData: ChartDataSets[] = [
     {
       data: [
-        this.valueStnd1,
-        this.valueStnd2,
-        this.valueStnd3,
-        this.valueStnd4
+        100,
+        150,
+        128,
+        80,
+        85,
+        94
       ],
       label: 'Blutzuckerspiegel'
     },
-  ];
+  ]
 
-  lineChartLabels: Label[] = ['07:00', '08:00', '09:00', '10:00'];
-
-  /*lineChartOptions = {
-    responsive: true,
-  };*/
-
-
+  lineChartLabels: Label[] = ['07:00', '08:00', '09:00', '10:00', '11:00', '12:00']
 
   lineChartColors: Color[] = [
     {
-      borderColor: 'red',
-      backgroundColor: 'rgba(100, 300, 100,0.5)',
+      borderColor: 'rgba(22, 16, 50,0.7)',
+      backgroundColor: 'rgba(255, 196, 9,0.7)',
     },
-  ];
+  ]
 
-  lineChartLegend = true;
-  lineChartPlugins = [];
-  lineChartType = 'line';
+  lineChartLegend = true
+  lineChartPlugins = []
+  lineChartType = 'line'
 
   public lineChartOptions: ChartOptions = {
     responsive: true,
@@ -112,11 +139,12 @@ export class Tab2Page {
         }
       }]
     }
-  };
+  }
 
-
-
+  /**
+   * Öffnet den Infodialog
+   */
   infoPressed() {
-    console.log("Infobutton pressed");
+    console.log("Infobutton pressed")
   }
 }
