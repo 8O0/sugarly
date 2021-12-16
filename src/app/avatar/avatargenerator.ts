6ac06a585127a2842ed28e0f6ba31ebf994f2acf
Nikola Stanic <stann1@bfh.ch>
2021-12-16 20:07:17 +0100

    Structure Change SVG

--- src/app/avatar/avatargenerator.ts
+++ src/app/avatar/avatargenerator.ts
@@ -4,11 +4,11 @@ import { Avatar } from "./avatar";
  * Generiert die Avatare
  */
 export const AVATAR_COLLECTION = new Map <string,Avatar>([
-    ["nina", new Avatar ("Nina", "w", "assets/avatar_Img/Nina_Default.svg", 
-    "assets/avatar_Img/Nina_Grin.svg","assets/avatar_Img/Nina_Love.svg",
-    "assets/avatar_Img/Nina_Smile.svg","assets/avatar_Img/Nina_Talking.svg")],
-    ["tom", new Avatar ("Tom", "m", "assets/avatar_Img/Tom_Default.svg", 
-    "assets/avatar_Img/Tom_Grin.svg","assets/avatar_Img/Tom_Love.svg",
-    "assets/avatar_Img/Tom_Smile.svg","assets/avatar_Img/Tom_Talking.svg")]
+    ["nina", new Avatar ("Nina", "w", "assets/imgs/avatar_Img/Nina_Default.svg", 
+    "assets/imgs/avatar_Img/Nina_Grin.svg","assets/imgs/avatar_Img/Nina_Love.svg",
+    "assets/imgs/avatar_Img/Nina_Smile.svg","assets/imgs/avatar_Img/Nina_Talking.svg")],
+    ["tom", new Avatar ("Tom", "m", "assets/imgs/avatar_Img/Tom_Default.svg", 
+    "assets/imgs/avatar_Img/Tom_Grin.svg","assets/imgs/avatar_Img/Tom_Love.svg",
+    "assets/imgs/avatar_Img/Tom_Smile.svg","assets/imgs/avatar_Img/Tom_Talking.svg")]
 ]);
     
