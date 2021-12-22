dd7ccda7de856b1f04870174b752a50283f1cd75
8O0 <lukas.loosli@hotmail.de>
2021-12-22 18:20:18 +0100

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'askingavatar',
     loadChildren: () => import('./askingavatar/askingavatar.module').then( m => m.AskingavatarPageModule)
   },
