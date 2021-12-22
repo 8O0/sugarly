67a31404e0483c18cfacd260c92399d8e4ba20b0
rohrj7 <rohrj7@bfh.ch>
2021-12-22 13:54:06 +0100

    Score Component new

---
+++ src/app/tab3-answer/tab3-answer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3-answer',
+  templateUrl: './tab3-answer.component.html',
+  styleUrls: ['./tab3-answer.component.scss'],
+})
+export class Tab3AnswerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/tab3-score/tab3-score.component.ts
+++ src/app/tab3-score/tab3-score.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-tab3-score',
@@ -10,5 +11,6 @@ export class Tab3ScoreComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+
 
 }
